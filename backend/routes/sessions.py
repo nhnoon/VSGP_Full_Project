061c77bf44c97463a1d07e75d1e8d6606e0eca16
Masaher Alshamrani <masaheer.alshamrani@gmail.com>
2025-12-16 03:14:36 +0300

    Fix sessions backend and prepare for submission

--- backend/routes/sessions.py
+++ backend/routes/sessions.py
@@ -1,3 +1,5 @@
+
+# temporary change for commit
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from datetime import datetime, timedelta
